76aa978919fad98944c7997ab92174dbb6b05a89
Vladimir_rekai <vladimir.rekai.etu.unicaen.fr>
2024-03-22 10:42:51 +0100

    feat: view and function getRankings created

--- src/composable/supabase.js
+++ src/composable/supabase.js
@@ -1,6 +1,17 @@
+import { createClient } from '@supabase/supabase-js'
+
 export default function useSupabase(){
     const url = 'https://uggqhbotmjvpabwhzamp.supabase.co'
     const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZ3FoYm90bWp2cGFid2h6YW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NTQ3NjEsImV4cCI6MjAyNTIzMDc2MX0.dQFV1slHWmtJy3NhDcGazABgI9YWkotBLenO_WRMOPg'
 
-    
+    const supabase = createClient(url, anon)
+
+    async function getRankings(){
+        const { data, error } = await supabase
+            .from('team')
+            .select()
+        return data
+    }
+
+    return {getRankings}
 }
---
+++ src/views/Rankings.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import useSupabase from '@/composable/supabase';
+
+    const { getRankings } = useSupabase();
+
+    let rankings = ref([]);
+
+    onMounted(async () => {
+        rankings.value = await getRankings();
+        console.log(profiles.value);
+    })
+
+</script>
+
+<template>
+    <h1>Stylé</h1>
+</template>
