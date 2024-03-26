4d123ef34b4fdae1a53c01e281ff8da4d3b4b996
riguet222 <riguet222@unicaen.fr>
2024-03-26 11:15:59 +0100

    feat: complete implementation of creation of match

--- src/components/MatchInput.vue
+++ src/components/MatchInput.vue
@@ -27,17 +27,30 @@
     const selectedSport = ref(null);
     const matchTime = ref("");
 
+
     async function submitMatch() {
+        console.log(team1)
+        console.log( getTeamIdByName(team1.value))
         const matchData = {
             mat_id: getNewId(),
-            mat_start_time: matchTime.value.toString,
-            spo_id: selectedSport.value,
-            tea_id_1: team1.value,
-            tea_id_2: team2.value
-        };
+            mat_start_time: matchTime.value.toString(),
+            spo_id: getSportIdByName(selectedSport.value),4
+            tea_id_1: getTeamIdByName(team1.value),
+            tea_id_2: getTeamIdByName(team2.value)
+        }
         await setMatch(matchData);
 
         emit('close')
+    }
+
+    function getSportIdByName(sportName) {
+        const sport = listOfSports.value.find(sport => sport.spo_name === sportName);
+        return sport ? sport.spo_id : null;
+    }
+
+    function getTeamIdByName(teamName) {
+        const team = listOfTeams.value.find(team => team.tea_name === teamName);
+        return team ? team.tea_id : null;
     }
 
     function getNewId(){
@@ -56,22 +69,22 @@
 <template>
 
     <div class="match">
-        <select name="Team 1">
+        <select name="Team 1" v-model="team1">
             <option v-for="team in listOfTeams" :key=team.tea_id>
                 {{ team.tea_name }}
             </option>
         </select>
-        <select name="Team 2">
+        <select name="Team 2" v-model="team2">
             <option v-for="team in listOfTeams" :key=team.tea_id>
                 {{ team.tea_name }}
             </option>
         </select>
-        <select name="Sport">
+        <select name="Sport" v-model="selectedSport">
             <option v-for="sport in listOfSports" :key=sport.spo_id>
                 {{ sport.spo_name }}
             </option>
         </select>
-        <input type="time" class="text-black" placeholder="Temps"></input>
+        <input type="time" class="text-black" placeholder="Temps" v-model="matchTime"></input>
     </div>
     <button @click="submitMatch">Create the match</button>
 </template>
