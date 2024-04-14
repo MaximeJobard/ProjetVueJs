d5a89b830d34a26808335d6907d451a03a45e8f6
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2024-04-14 22:44:02 +0200

    refactor: changed href to router.push

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,17 +16,33 @@
 		router.push("/")
 	}
 
+	function goToRankings(){
+		router.push('/rankings')
+	}
+
+	function goToTeam(){
+		router.push('/managementTeam')
+	}
+
+	function goToMatch(){
+		router.push('/createMatch')
+	}
+
+	function gotToSignIn(){
+		router.push("/")
+	}
+
 </script>
 
 <template>
 	<header class="w-full bg-gray-800 text-white p-4">
 		<nav>
 		<ul class="flex justify-evenly items-center">
-			<li><a href="/rankings">Rankings</a></li>
-			<li v-if="isLogged"><a  href="/managementTeam">Management Team</a></li>
-			<li v-if="isLogged"><a href="/createMatch">Create Match</a></li>
+			<li><button @click="goToRankings">Rankings</button></li>
+			<li v-if="isLogged"><button @click="goToTeam">Management Team</button></li>
+			<li v-if="isLogged"><button @click="goToMatch">Create Match</button></li>
 			<li v-if="isLogged"><button class="inline-block bg-red-500 text-white px-4 py-2 rounded" @click="_signOut">Sign out</button></li>
-			<li v-if="!isLogged"><a href="/">Sign in</a></li>
+			<li v-if="!isLogged"><button @click="gotToSignIn">Sign in</button></li>
 		</ul>
 		</nav>
 	</header>
